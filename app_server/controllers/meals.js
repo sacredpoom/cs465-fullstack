b14f6e3fe6a221cdca86100aa5242420ca80d1e5
sacredpoom <jcvenesk@gmail.com>
2024-03-22 00:10:37 -0400

    Module 3 Completed Baseline

--- app_server/controllers/meals.js
+++ app_server/controllers/meals.js
@@ -1,6 +1,9 @@
+var fs = require('fs');
+var meal = JSON.parse(fs.readFileSync('./data/meal.json', 'utf8'));
+
 /* GET meals view */
 const meals = (req, res) => {
-    res.render('meals', { title: 'Travlr Getaways'});
+    res.render('meals', { title: 'Travlr Getaways', meal});
 };
 
 module.exports = {
--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,6 +1,9 @@
+var fs = require('fs');
+var room = JSON.parse(fs.readFileSync('./data/room.json', 'utf8'));
+
 /* GET rooms view */
 const rooms = (req, res) => {
-    res.render('rooms', { title: 'Travlr Getaways'});
+    res.render('rooms', { title: 'Travlr Getaways', room});
 };
 
 module.exports = {
